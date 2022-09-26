d9a280ccd2cd26c0b6aad9da734406aed9763694
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-09-26 22:15:41 +0300

    finalal commit1

--- HW1.py
+++ HW1.py
@@ -73,10 +73,10 @@
 # Напишите программу для. проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-print('x y z f1   f2')
-for x in range(2):
-   for y in range(2):
-       for z in range(2):
-           f1 = not(x or y or z)
-           f2 = (not x) and (not y) and (not z) 
-           print(x, y, z, bool(f1), bool(f2))
+# print('x y z f1   f2')
+# for x in range(2):
+#    for y in range(2):
+#        for z in range(2):
+#            f1 = not(x or y or z)
+#            f2 = (not x) and (not y) and (not z) 
+#            print(x, y, z, bool(f1), bool(f2))
