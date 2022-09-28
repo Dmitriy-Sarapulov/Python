3521fdfe698605f6dd048ab78ab161104971847c
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-09-28 19:13:50 +0300

    second commit

--- lec2.py
+++ lec2.py
@@ -1,2 +1,17 @@
 # Данные, функции и модули в Python.
 
+#  режимы для записи данныхЖ
+
+#a - 
+#r - 
+#w - 
+
+# colors = ['red', ' green', 'blue'] # запись данных в файл
+# data = open('file.txt', 'a')
+# data.writelines(colors)
+# data.close()
+
+# with open('file.exe', 'a') as data:
+#     data.write('line 1111\n')
+#     data.write('line 1221\n')
+
