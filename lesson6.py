b42d568f39ffb8240c165ff4d8f43dd70aa6bd35
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-10-10 21:49:58 +0300

    final commit

--- lesson6.py
+++ lesson6.py
@@ -1,1 +1,37 @@
 # Урок 6. Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension. Продолжение
+
+# lst = [1, 1, 2, 3, 5, 1, 5, 3, 10] #-> [2, 10]
+
+# def sort(lst):
+#     uniq_elements = set()
+#     for el in list:
+#         if el not in uniq_elements:
+#             uniq_elements.add(el)
+#         else:
+#             uniq_elements.discard(el)
+#     s = list(uniq_elements)
+#     s.sort()
+#     return s
+
+#'(1+2)*3'
+
+# def parse(s):
+#     result = []
+#     for symb in s:
+#         if symb.isdigit():
+#             digit +=symb
+#         elif symb in ['(', ')']:
+#             if digit:
+#                 result.append(float(digit))
+#                 digit = ''
+#             result.append(symb)
+#         else:
+#             if digit:
+#                 result.append(float(digit))
+#                 digit = ''
+#             result.append(symb)
+#     else:
+#         if digit:
+#             result.append(float(digit))
+#     return result
+# print(parse("(1+2)*3"))
