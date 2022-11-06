bc56a23f32939a7b78eff7551e22ea37bc359553
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-11-06 20:14:49 +0300

    final commit

--- lesson8.py
+++ lesson8.py
@@ -138,3 +138,136 @@ mc = MyClass(100)
 print(mc.val)
 mc += 200
 
+# Интерфейсы. Абстрактные классы.
+
+from abc import ABC, abstractclassmethod # без мпорта не сработает. Встроеный декоратор.
+
+class MyAbstractClass(ABC):
+
+    @abstractclassmethod # Напоминание о переопределении.Контролирует переопределен ли данный метод.
+    def my_method_1(self):
+        pass
+
+    @abstractclassmethod # если есть декоратор абстрактного класса то переопределение обязательно
+    def my_method_2(self):
+        pass
+
+class MyClass(MyAbstractClass):
+
+    def my_method_1(self):
+        pass
+
+    def my_method_2(self):
+        pass
+
+mc = MyClass()
+print(type(mc.my_method_2))
+
+# @property - Функция property() используется для определения свойств в классах. Метод property() 
+# обеспечивает интерфейс для атрибутов экземпляра класса. 
+# Он инкапсулирует атрибуты экземпляров и предоставляет свойства, аналогично тому, как это работает в Java и C#.
+
+class MyClass:
+    def __init__(self, param_1, param_2):
+        self.param_1 = param_1
+        self.param_2 = param_2
+
+    @property # Мы будем работать с этим методом, как с атрибутом. Стилевая опция.
+    def my_method(self):
+        return f"Параметры, переданные в класс: " \
+            f"{self.param_1}, {self.param_2}"
+
+mc = MyClass()
+print(mc.my_method())
+
+# Статические методы и методы класса
+
+class Auto:
+    @staticmethod # Что бы не передавать self, только за счёт обертывания.(убрать лишнее, логически)
+    def get_class_info(self): #self передаем всегда!!! пердавать нужно, но он не используется
+        print("Детальная информация о классе")
+
+a = Auto()
+a.get_class_info()
+
+# if __name__ == '__main__': - запсук модуля,как самостоятельной программы.
+
+if __name__ == '__main__': # если мы запускаем модуль как самостоятельную программу, то:
+    a = Auto() # Надо вот это выполнять.
+    a.get_class_info()
+
+# @classmethod
+
+class User:
+    pass
+
+class NewClass:
+    pass
+
+class MyClass:
+    @classmethod # Если им обернуть метод класса, то можно вызвать его через объект или сам класс.
+    def my_method(cls): # метод класса (cls) - указание на класс
+        print(cls.__dict__)
+
+MyClass.my_method() # вызов метода через название класса
+mc = MyClass()
+print(mc.my_method())
+
+# Атрибуты и встроенные методы объектов классов
+
+class User:
+    def __init__(self, name, login, passwd, email):
+        self.name = name
+        self.login = login
+        self.passwd = passwd
+        self.email = email
+    
+    def on_get_data(self):
+        print(f"имя: {self.name}, логин: {self.login}, "
+        f"пароль: {self.passwd}, email: {self.email}")
+    
+u = User('Ivan Ivanov', "IvIv", "11111", "iviv@mail.ru")
+u.on_get_data()
+print(f" name - {User.name}, \n module - {User.__module__}, \n"
+f"__dict__ - {User.__dict__}, \n __bases__ - {User.__bases__}, \n"
+f"__doc__ - {User.__doc__}, \n hash - {User.__hash__}") # это все характеристики объекта.
+
+# Пример приложения с ООП
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f"Name and Surname: {self.name}, {self.surname}"
+
+class Teacher(Person):
+    def to_teach(self, subj, *pupils):
+        for pupil in pupils:
+            pupil.to_take(subj)
+
+class Pupil(Person):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.knowledges = []
+
+    def to_take(self, subj):
+        self.knowledges.append(subj)
+
+class Subject:
+    def __init__(self, *subjects):
+        self.subjects = list(subjects)
+
+s = Subject("maths", "phisics", "chemistry")
+t = Teacher("Ivan", "Ivanov")
+print(t)
+
+p_1 = Pupil("Petr", "Petrov")
+p_2 = Pupil("Sergey", "Sergeev")
+p_3 = Pupil("Vladimir", "Vladimirov")
+print(f'{p_1}, {p_2}, {p_3}')
+
+t.to_teach(s, p_1, p_2, p_3)
+print(p_1.knowledges[0].my_list())
+
